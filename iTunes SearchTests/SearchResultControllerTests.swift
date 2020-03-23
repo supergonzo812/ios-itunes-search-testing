5b21a4029319f6005e0b76af74de1bbc50e69b56
Chris <chris.gonzales.82@gmail.com>
2020-03-23 09:32:17 -0700

    Added a test with an expecation

--- iTunes SearchTests/SearchResultControllerTests.swift
+++ iTunes SearchTests/SearchResultControllerTests.swift
@@ -7,9 +7,33 @@
 //
 
 import XCTest
+@testable import iTunes_Search
+/*
+ 
+ Does decoding work?
+ Does decoding fail when given bad data?
+ Does it build the correct URL?
+ Does it build the correct URLRequest?
+ Are the search results saved properly?
+ Is the completion handler called when data is good?
+ Is the completion handler called when data is bad?
+ Is the completion handler called when networking fails?
+ 
+ */
 
 class SearchResultControllerTests: XCTestCase {
 
-    
+    func testForSomeResults() {
+        let controller = SearchResultController()
+        
+        let expectation = self.expectation(description: "Wait for results")
+        controller.performSearch(for: "GarageBand",
+                                 resultType: .software) {
+                                    expectation.fulfill()
+        }
+        
+        wait(for: [expectation],
+             timeout: 5)
+    }
 
 }
