4ce0ae5143255d666010fe67ba099b00ffdb5fb0
Chris <chris.gonzales.82@gmail.com>
2020-03-23 09:18:18 -0700

    Added a unit testing target to the project

---
+++ iTunes SearchTests/SearchResultControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  iTunes_SearchTests.swift
+//  iTunes SearchTests
+//
+//  Created by Chris Gonzales on 3/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class SearchResultControllerTests: XCTestCase {
+
+    
+
+}
